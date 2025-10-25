42d3179ffe766804a482fdd1996359a48c5392ee
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2025-10-24 23:00:50 -0400

    migracion de la BD

--- app/src/main/java/com/example/ucbp1/features/movie/data/database/AppRoomDataBase.kt
+++ app/src/main/java/com/example/ucbp1/features/movie/data/database/AppRoomDataBase.kt
@@ -4,6 +4,8 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.room.migration.Migration
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.ucbp1.features.movie.data.database.dao.IMovieDao
 import com.example.ucbp1.features.movie.data.database.entity.MovieEntity
 
@@ -17,6 +19,16 @@ abstract class AppRoomDataBase : RoomDatabase() {
     abstract fun movieDao(): IMovieDao
 
     companion object {
+        private val MIGRATION_1_2 = object : Migration(1, 2) {
+            override fun migrate(db: SupportSQLiteDatabase) {
+                db.execSQL("ALTER TABLE `movies_table` ADD COLUMN `title` TEXT NOT NULL DEFAULT ''")
+                db.execSQL("ALTER TABLE `movies_table` ADD COLUMN `overview` TEXT NOT NULL DEFAULT ''")
+                db.execSQL("ALTER TABLE `movies_table` ADD COLUMN `posterPath` TEXT") // Este es nullable
+                db.execSQL("ALTER TABLE `movies_table` ADD COLUMN `releaseDate` TEXT") // Este es nullable
+                db.execSQL("ALTER TABLE `movies_table` ADD COLUMN `isLiked` INTEGER NOT NULL DEFAULT 0")
+                db.execSQL("ALTER TABLE `movies_table` ADD COLUMN `userRating` INTEGER NOT NULL DEFAULT 0")
+            }
+        }
         @Volatile
         private var INSTANCE: AppRoomDataBase? = null
 
@@ -27,7 +39,7 @@ abstract class AppRoomDataBase : RoomDatabase() {
                     AppRoomDataBase::class.java,
                     "movie_feature_database"
                 )
-                    .fallbackToDestructiveMigration()
+                    .addMigrations(MIGRATION_1_2)
                     .build()
                 INSTANCE = instance
                 instance
