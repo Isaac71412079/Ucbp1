440d7ac245900952b9dc15ae5b13531907d1f346
Isaac71412079 <isaac.rivero@ucb.edu.bo>
2025-10-25 08:08:03 -0400

    pregunta 2 examen 2 splash

--- app/src/main/java/com/example/ucbp1/features/splash/SplashScreen.kt
+++ app/src/main/java/com/example/ucbp1/features/splash/SplashScreen.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.size
+import androidx.compose.material.icons.Icons
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,40 +20,34 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
-import com.example.ucbp1.R // Asegúrate que sea el R de tu proyecto
+import com.example.ucbp1.R
 import com.example.ucbp1.navigation.Screen
 import kotlinx.coroutines.delay
-
+import androidx.compose.material.icons.filled.MenuBook // Importa el icono del libro
 @Composable
 fun SplashScreen(navController: NavController) {
 
-    // Este LaunchedEffect ejecuta el código de su bloque solo una vez
-    // cuando el Composable entra en la composición.
     LaunchedEffect(key1 = true) {
-        // Espera 3 segundos (3000 milisegundos)
         delay(3000)
-        // Navega a la pantalla de Login y limpia el stack para
-        // que el usuario no pueda volver al SplashScreen presionando "atrás".
-        navController.popBackStack() // Limpia el splash de la pila
-        navController.navigate(Screen.Login.route) // Navega al Login
+        navController.popBackStack()
+        navController.navigate(Screen.Login.route)
     }
 
-    // El diseño de la pantalla
     Column(
-        modifier = Modifier.fillMaxSize(),
+        modifier = Modifier
+            .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-        // 1. El icono de tu app
-        Image(
-            painter = painterResource(id = R.mipmap.mobileback4), // Usa el icono de tu app
+        Icon(
+            imageVector = Icons.Default.MenuBook,
             contentDescription = "Logo de la App",
-            modifier = Modifier.size(150.dp) // Tamaño grande
+            modifier = Modifier.size(150.dp),
+            tint = MaterialTheme.colorScheme.primary
         )
 
         Spacer(modifier = Modifier.height(24.dp))
 
-        // 2. El título "ucbp1" debajo
         Text(
             text = "ucbp1",
             fontSize = 32.sp,
